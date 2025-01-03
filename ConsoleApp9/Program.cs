8057a3097bd2851d4ca624a0f4683f1214b73327
sukra <sukra@DESKTOP-OB7AJV3>
2025-01-03 21:53:29 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+if (sayi == 10)
+{
+    Console.WriteLine("girilen sayı 10'a eşittir.");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("girilen sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine("girilen sayı 10'dan büyüktür");
+}
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+
+}
+else
+{
+    Console.WriteLine("girilen sayı tektir");
+}
+
